ca56d9f0521b4873175f334ad625b24a76500b7f
Jetsada Machom <zinuzoid@gmail.com>
2015-12-30 17:51:00 +0700

    Add simple function

--- app/src/main/java/com/zinuzoid/zlog/example/MainActivity.java
+++ app/src/main/java/com/zinuzoid/zlog/example/MainActivity.java
@@ -3,11 +3,15 @@ package com.zinuzoid.zlog.example;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.zinuzoid.zlog.ZLog;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ZLog.d("Test");
 	}
 }
---
+++ zlog/src/main/java/com/zinuzoid/zlog/ZLog.java
@@ -0,0 +1,17 @@
+package com.zinuzoid.zlog;
+
+import android.util.Log;
+
+/**
+ * ZLog
+ * on 12/30/2015 AD
+ *
+ * @author Jetsada Machom <jim@imjim.im>
+ */
+public class ZLog {
+
+	public static void d(String message) {
+		Log.d("ZLog", message);
+	}
+
+}
