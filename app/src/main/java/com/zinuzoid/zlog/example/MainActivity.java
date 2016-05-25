5246a54295149a5dd9e882eecb1070b4151d94e2
Jetsada Machom <zinuzoid@gmail.com>
2016-05-25 12:49:17 +0700

    Update example

--- app/src/main/java/com/zinuzoid/zlog/example/MainActivity.java
+++ app/src/main/java/com/zinuzoid/zlog/example/MainActivity.java
@@ -2,6 +2,7 @@ package com.zinuzoid.zlog.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.zinuzoid.zlog.ZLog;
 
@@ -12,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		ZLog.setLogInterface(new ZLog.LogInterface() {
+			@Override
+			public void d(String tag, String message) {
+				Log.d(tag, message);
+			}
+		});
+
 		ZLog.d("Test");
+
+		ZLog.d(new RuntimeException("panic!"));
 	}
 }
